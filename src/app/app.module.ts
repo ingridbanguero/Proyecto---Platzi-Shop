ac5cf6c86f14d466475b016f40e274aa2fdbf46d
Ingrid Banguero <ingrid_banguero@hotmail.com>
2020-10-18 18:35:38 -0500

    home: create banners section with swiper library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,10 @@ import { HighlightDirective } from './highlight.directive';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
-import { DemoComponent } from './demo/demo.component'; // Modulo importando
+import { DemoComponent } from './demo/demo.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component'; // Modulo importando
 
 @NgModule({
   declarations: [ // Nuestros componentes
@@ -25,7 +28,10 @@ import { DemoComponent } from './demo/demo.component'; // Modulo importando
     HomeComponent,
     ProductsComponent,
     ContactComponent,
-    DemoComponent // Agrego mi modulo creado (contiene las variables a usar)
+    DemoComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent // Agrego mi modulo creado (contiene las variables a usar)
   ],
   imports: [ // Nuestros modulos
     BrowserModule,
